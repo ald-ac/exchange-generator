103ca4fd5c3929271d6778f4401c97b421347954
ald-ac <muriasaldair@hotmail.com>
2021-08-19 12:08:45 -0500

    First commit

---
+++ src/test/java/com/ald/exchangegenerator/app/ExchangeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ald.exchangegenerator.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
