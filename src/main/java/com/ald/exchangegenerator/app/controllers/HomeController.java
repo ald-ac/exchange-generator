fba93f8758bb7536efc9e83bb30efc3cb1121608
ald-ac <muriasaldair@hotmail.com>
2021-08-19 14:39:09 -0500

    Create contestant list to set like model attribute

--- src/main/java/com/ald/exchangegenerator/app/controllers/HomeController.java
+++ src/main/java/com/ald/exchangegenerator/app/controllers/HomeController.java
@@ -2,9 +2,19 @@ package com.ald.exchangegenerator.app.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.ald.exchangegenerator.app.models.domain.ContestantList;
 
 @Controller
+@SessionAttributes({"contestants"})
 public class HomeController {
+	
+	@ModelAttribute("contestants")
+	public ContestantList contestants() {
+		return new ContestantList();
+	}
 	
 	@GetMapping("/")
 	public String home() {
---
+++ src/main/java/com/ald/exchangegenerator/app/models/domain/ContestantList.java
@@ -0,0 +1,7 @@
+package com.ald.exchangegenerator.app.models.domain;
+
+import java.util.ArrayList;
+
+public class ContestantList extends ArrayList<Contestant>{
+	
+}
